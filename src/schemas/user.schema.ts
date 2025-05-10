820848f0f44faf624237146da3198b1372ab5af6
zakawa <uossofzaki@gmail.com>
2025-05-10 20:15:40 +0300

    last commit

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
